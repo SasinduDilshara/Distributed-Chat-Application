d0f3b8de8f20a75ad6d0a4b5132b4bcdb3e52d80
Damika Gamlath <damika97.10@gmail.com>
2022-02-19 21:05:04 +0530

    server config load

--- chat-server-app/src/main/java/com/ds/chatserver/ChatServerApplication.java
+++ chat-server-app/src/main/java/com/ds/chatserver/ChatServerApplication.java
@@ -2,10 +2,12 @@ package com.ds.chatserver;
 
 import com.ds.chatserver.chatroom.ChatRoomHandler;
 import com.ds.chatserver.clienthandler.ClientRequestHandler;
+import com.ds.chatserver.config.ServerConfigurations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
@@ -13,8 +15,10 @@ public class ChatServerApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         logger.info("Application Started");
+        ServerConfigurations.loadServerDetails("conf/conf.txt");
+        logger.info("Configuration file loaded");
         ChatRoomHandler chatRoomHandler = ChatRoomHandler.getInstance();
         try {
             ClientRequestHandler clientRequestHandler = new ClientRequestHandler(chatRoomHandler);
--- chat-server-app/src/main/java/com/ds/chatserver/config/ServerConfigurations.java
+++ chat-server-app/src/main/java/com/ds/chatserver/config/ServerConfigurations.java
@@ -2,22 +2,40 @@ package com.ds.chatserver.config;
 
 import com.ds.chatserver.serverhandler.ServerDetails;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ServerConfigurations {
     private static HashMap<String, ServerDetails> serverDetails;
 
     public static ServerDetails getServerDetails(String serverId) {
-        return null;
+        return serverDetails.get(serverId);
     }
 
-    public void setServerDetails(HashMap<String, ServerDetails> serverDetails) {
-        this.serverDetails = serverDetails;
+    public static int getNumberOfServers(){
+        return serverDetails.size();
     }
 
-    public static void loadServerDetails() {
-        //TODO: read file and append each server details as ServerDetails object to this.serverDetails arraylist
-        serverDetails = null;
+    public static void loadServerDetails(String filePath) throws
+            FileNotFoundException,
+            ArrayIndexOutOfBoundsException,
+            NumberFormatException {
+
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+        serverDetails = new HashMap<String, ServerDetails>();
+
+        while (sc.hasNextLine()){
+            String[] tokens = sc.nextLine().split("\t");
+            ServerDetails sd = new ServerDetails(
+                    tokens[0],
+                    tokens[1],
+                    Integer.parseInt(tokens[2]),
+                    Integer.parseInt(tokens[3]));
+            serverDetails.put(tokens[0], sd);
+        }
     }
 }
