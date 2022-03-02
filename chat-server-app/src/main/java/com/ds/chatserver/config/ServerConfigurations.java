76f86e29e22f8316d53ca27b1b7834f609c43b18
ruchin <raruchin@gmail.com>
2022-03-02 13:14:32 +0530

    ServerConfiguration file refactor

--- chat-server-app/src/main/java/com/ds/chatserver/config/ServerConfigurations.java
+++ chat-server-app/src/main/java/com/ds/chatserver/config/ServerConfigurations.java
@@ -4,15 +4,19 @@ import com.ds.chatserver.serverhandler.ServerDetails;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+
+import static com.ds.chatserver.constants.ServerConfigurationConstants.*;
 
 public class ServerConfigurations {
     private static HashMap<String, ServerDetails> serverDetails;
 
     public static ServerDetails getServerDetails(String serverId) {
         return serverDetails.get(serverId);
+    }
+
+    public static Set<String> getServerIds() {
+        return serverDetails.keySet();
     }
 
     public static int getNumberOfServers(){
@@ -30,12 +34,15 @@ public class ServerConfigurations {
 
         while (sc.hasNextLine()){
             String[] tokens = sc.nextLine().split("\t");
-            ServerDetails sd = new ServerDetails(
-                    tokens[0],
-                    tokens[1],
-                    Integer.parseInt(tokens[2]),
-                    Integer.parseInt(tokens[3]));
-            serverDetails.put(tokens[0], sd);
+
+            ServerDetails sd = ServerDetails.builder()
+                    .serverId(tokens[SERVER_ID])
+                    .ipAddress(tokens[IP_ADDRESS])
+                    .serverPort(Integer.parseInt(tokens[SERVER_PORT]))
+                    .clientPort(Integer.parseInt(tokens[CLIENT_PORT]))
+                    .build();
+
+            serverDetails.put(tokens[SERVER_ID], sd);
         }
     }
 }
