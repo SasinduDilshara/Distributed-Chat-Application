170dbb8f0432dcb9dbbfb2618df1c00ceb016271
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2022-03-07 22:43:32 +0530

    Update event attributres

--- chat-server-app/src/main/java/com/ds/chatserver/log/Event.java
+++ chat-server-app/src/main/java/com/ds/chatserver/log/Event.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Event {
+    private String clientId;
+    private String serverId;
     private EventType type;
     private int logIndex;
     private int logTerm;
+    private String parameter;
 }
