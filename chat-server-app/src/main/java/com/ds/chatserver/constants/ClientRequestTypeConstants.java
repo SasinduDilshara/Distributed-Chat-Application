d9caa88941365815ef840630be166abbd2eed64a
gayal <g.c.dassanayake@gmail.com>
2022-03-14 15:36:17 +0530

    Define constants for ServerMessage strings

--- chat-server-app/src/main/java/com/ds/chatserver/constants/ClientRequestTypeConstants.java
+++ chat-server-app/src/main/java/com/ds/chatserver/constants/ClientRequestTypeConstants.java
@@ -10,4 +10,9 @@ public class ClientRequestTypeConstants {
     public static final String DELETE_ROOM = "deleteroom";
     public static final String MESSAGE = "message";
     public static final String QUIT = "quit";
+    public static final String ROOM_LIST = "roomlist";
+    public static final String ROOM_CONTENTS = "roomcontents";
+    public static final String ROOM_CHANGE = "roomchange";
+    public static final String ROUTE = "route";
+    public static final String SERVER_CHANGE = "serverchange";
 }
--- chat-server-app/src/main/java/com/ds/chatserver/utils/ServerMessage.java
+++ chat-server-app/src/main/java/com/ds/chatserver/utils/ServerMessage.java
@@ -4,13 +4,16 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import java.util.ArrayList;
 
+import static com.ds.chatserver.constants.ClientRequestTypeConstants.*;
+import static com.ds.chatserver.constants.CommunicationProtocolKeyWordsConstants.*;
+
 public class ServerMessage {
 
     @SuppressWarnings("unchecked")
     public static JSONObject getNewIdentityResponse(Boolean approved) {
         JSONObject newIdentity = new JSONObject();
-        newIdentity.put("type", "newidentity");
-        newIdentity.put("approved", approved.toString());
+        newIdentity.put(TYPE, NEW_IDENTITY);
+        newIdentity.put(APPROVED, approved.toString());
         return newIdentity;
     }
 
@@ -18,9 +21,9 @@ public class ServerMessage {
     public static JSONObject getRoomListResponse(ArrayList<String> roomNames) {
         JSONObject list = new JSONObject();
         JSONArray rooms = new JSONArray();
-        list.put("type", "roomlist");
+        list.put(TYPE, ROOM_LIST);
         rooms.addAll(roomNames);
-        list.put("rooms", rooms);
+        list.put(ROOMS, rooms);
         return list;
     }
 
@@ -29,75 +32,75 @@ public class ServerMessage {
         JSONObject who = new JSONObject();
         JSONArray identities = new JSONArray();
         identities.addAll(clientNames);
-        who.put("type", "roomcontents");
-        who.put("roomid", roomId);
-        who.put("identities", identities);
-        who.put("owner", owner);
+        who.put(TYPE, ROOM_CONTENTS);
+        who.put(ROOM_ID, roomId);
+        who.put(IDENTITIES, identities);
+        who.put(OWNER, owner);
         return who;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getCreateRoomResponse(String roomid, Boolean approved) {
         JSONObject create_room = new JSONObject();
-        create_room.put("type", "createroom");
-        create_room.put("roomid", roomid);
-        create_room.put("approved", approved.toString());
+        create_room.put(TYPE, CREATE_ROOM);
+        create_room.put(ROOM_ID, roomid);
+        create_room.put(APPROVED, approved.toString());
         return create_room;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getRoomChangeResponse(String identity, String former, String roomid) {
         JSONObject change_room = new JSONObject();
-        change_room.put("type", "roomchange");
-        change_room.put("identity", identity);
-        change_room.put("former", former);
-        change_room.put("roomid", roomid);
+        change_room.put(TYPE, ROOM_CHANGE);
+        change_room.put(IDENTITY, identity);
+        change_room.put(FORMER, former);
+        change_room.put(ROOM_ID, roomid);
         return change_room;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getRouteResponse(String roomId, String host, String port) {
         JSONObject route = new JSONObject();
-        route.put("type", "route");
-        route.put("roomid", roomId);
-        route.put("host", host);
-        route.put("port", port);
+        route.put(TYPE, ROUTE);
+        route.put(ROOM_ID, roomId);
+        route.put(HOST, host);
+        route.put(PORT, port);
         return route;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getServerChangeResponse(String serverId) {
         JSONObject serverchange = new JSONObject();
-        serverchange.put("type", "serverchange");
-        serverchange.put("approved", "true");
-        serverchange.put("serverid", serverId);
+        serverchange.put(TYPE, SERVER_CHANGE);
+        serverchange.put(APPROVED, "true");
+        serverchange.put(SERVER_ID, serverId);
         return serverchange;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getDeleteRoomResponse(String roomId, Boolean approved) {
         JSONObject delete = new JSONObject();
-        delete.put("type", "deleteroom");
-        delete.put("roomid", roomId);
-        delete.put("approved", approved.toString());
+        delete.put(TYPE, DELETE_ROOM);
+        delete.put(ROOM_ID, roomId);
+        delete.put(APPROVED, approved.toString());
         return delete;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getDeleteRoomServerResponse(String serverId, String roomId) {
         JSONObject server_delete = new JSONObject();
-        server_delete.put("type", "deleteroom");
-        server_delete.put("serverid", serverId);
-        server_delete.put("roomId", roomId);
+        server_delete.put(TYPE, DELETE_ROOM);
+        server_delete.put(SERVER_ID, serverId);
+        server_delete.put(ROOM_ID, roomId);
         return server_delete;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getMessageResponse(String identity, String content) {
         JSONObject message = new JSONObject();
-        message.put("type", "message");
-        message.put("identity", identity);
-        message.put("content", content);
+        message.put(TYPE, MESSAGE);
+        message.put(IDENTITY, identity);
+        message.put(CONTENT, content);
         return message;
     }
 }
