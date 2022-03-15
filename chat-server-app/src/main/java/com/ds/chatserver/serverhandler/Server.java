d00e3a3e4bbc6adc0f96c1fe5c1e85ad9842d364
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2022-03-15 21:11:01 +0530

    Refactor server class

--- chat-server-app/src/main/java/com/ds/chatserver/serverhandler/Server.java
+++ chat-server-app/src/main/java/com/ds/chatserver/serverhandler/Server.java
@@ -1,27 +1,19 @@
 package com.ds.chatserver.serverhandler;
 
 import com.ds.chatserver.chatroom.ChatRoom;
-import com.ds.chatserver.chatroom.ChatRoomHandler;
 import com.ds.chatserver.config.ServerConfigurations;
-import com.ds.chatserver.exceptions.ChatroomAlreadyExistsException;
-import com.ds.chatserver.exceptions.ClientNotInChatRoomException;
-import com.ds.chatserver.exceptions.InvalidChatroomException;
 import com.ds.chatserver.log.RaftLog;
-import com.ds.chatserver.serverstate.CandidateState;
 import com.ds.chatserver.serverstate.FollowerState;
 import com.ds.chatserver.serverstate.ServerState;
 import com.ds.chatserver.systemstate.ChatroomLog;
 import com.ds.chatserver.systemstate.SystemState;
 import com.ds.chatserver.utils.Util;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Set;
 
 @Setter
@@ -85,5 +77,4 @@ public class Server {
         this.state.stop();
         this.state = state;
     }
-
 }
